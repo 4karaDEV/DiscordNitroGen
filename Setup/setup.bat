dc167d8a89c17eab9c10011ea5183fe9fcc80111
4kara <124724066+4karaDEV@users.noreply.github.com>
2023-03-03 19:49:37 +0100

    Add files via upload

---
+++ Setup/setup.bat
@@ -0,0 +1,8 @@
+@echo off
+title Setup - by 4karaDEV ^| https://github.com/4karaDEV
+echo.
+echo Downloading...
+pip install -U -r requirements.txt
+pause
+cd ..
+start Stormy.py
---
+++ launch.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+title Stormy
+python Stormy.py
+pause
