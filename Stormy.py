dde37827154f1284d26c38d1f6927d3bad020531
4kara <124724066+4karaDEV@users.noreply.github.com>
2023-03-04 22:07:41 +0100

    Update Stormy.py

--- Stormy.py
+++ Stormy.py
@@ -1,2 +1,13 @@
-🔌 demo file
-🛑 its illegal btw 🛑
+# 🔌 demo file
+# 🛑 its illegal btw 🛑
+
+def main():
+  print("demo file")
+  input("now rate my project because I spent a lot of time above ->")
+  print("will put a star")
+  name = input("I see you don't want to leave, what's your name?")
+  print("{name} its a beautiful name, well I see you're staying so I'm going to turn off by myself")
+  from time import sleep, time
+  sleep(4)
+  exit()
+  
